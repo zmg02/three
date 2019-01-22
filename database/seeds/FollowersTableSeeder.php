dcdff0bacad5f7fe3b98ad46489690a6ef2c8a17
zmg02 <1225798710@qq.com>
2019-01-22 07:23:50 +0000

    Add user following

---
+++ database/seeds/FollowersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class FollowersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::all();
+        $user = $users->first();
+        $user_id = $user->id;
+
+        //获取去除掉ID为1的所有用户ID数组
+        $followers = $users->slice(1);
+        $follower_ids = $followers->pluck('id')->toArray();
+
+        //关注除了 1 号用户以外的所有用户都来关注 1 号用户
+        foreach ($followers as $follower) {
+        	$follower->follow($user_id);
+        }
+    }
+}
